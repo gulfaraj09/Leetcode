d0a97a9f9f25ef33e9a487565430032efe8931ed
md__gulfaraj__ <94758299+gulfaraj09@users.noreply.github.com>
2026-06-30 23:27:00 +0530

    Time: 0 ms (100%), Space: 8.1 MB (80.54%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.cpp
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -5,7 +5,6 @@ public:
         for(int i:word){
             if(i>=65 and i<=90)++count;
         }
-        cout<<count;
         return (count == len || 0 == count || (1 == count && (word[0]>=65 && word[0]<=90)));
     }
 };
