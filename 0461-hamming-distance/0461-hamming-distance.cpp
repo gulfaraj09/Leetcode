80614e4aff9147875ff2b2c0ce71c4914271c393
md__gulfaraj__ <94758299+gulfaraj09@users.noreply.github.com>
2026-07-11 23:50:42 +0530

    Time: 0 ms (100%), Space: 8 MB (37.3%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.cpp
+++ 0461-hamming-distance/0461-hamming-distance.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int hammingDistance(int x, int y) {
-        int count = 0;
-        for(int val = x^y; val>0; count++){
-            val = val & (val-1);
-        }
-        return count;
+        return __builtin_popcount(x ^ y);
     }
 };
